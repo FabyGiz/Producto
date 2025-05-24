703415b2ffac6731e896c112380e36344198ee02
FabyGiz <friveram@ucenfotec.ac.cr>
2025-05-24 09:14:36 -0600

    Creacion de estructua de file manager

---
+++ DataPersistence1/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistence1
+{
+    public class FileManager
+    {
+
+        public List<Product> ReadProducts() {
+            //Leer lista de productos de un txt y cargarlo en una lista
+            var products = new List<Product>();
+            return products;
+
+
+        }
+
+        public void SaveProduct(Product product) {
+            //Guardar el producto en un txt
+
+    }
+}
+}
