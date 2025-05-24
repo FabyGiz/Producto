446c852f3dc0fdd32323a86f5312bf4ff565547b
FabyGiz <friveram@ucenfotec.ac.cr>
2025-05-24 09:30:41 -0600

    Creacion del main

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+       public static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido al gestor de productos Dummy");
+        Console.WriteLine("Digite la opcion deseada:");
+        Console.WriteLine("1.Agregar un producto");
+        Console.WriteLine("2.Consultar listado de productos");
+
+
+        var option  = Int32.Parse(Console.ReadLine());
+
+        switch (option) {
+            case 1:
+                Console.WriteLine("Digite la informacion del producto a continuar");     
+                break;
+
+            case 2: Console.WriteLine("Listado de productos existentes");
+                break;
+
+            default:
+                Console.WriteLine("Opcion incorrecta");
+                break;
+;
+
+    }
+
+}
+}
