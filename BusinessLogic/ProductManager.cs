7e0003ece2f09a88c2aa93b41cfb7e5b8cb63af6
FabyGiz <friveram@ucenfotec.ac.cr>
2025-05-24 17:07:53 -0600

    Creacion de la conexion de program a business logic

--- BusinessLogic/ProductManager.cs
+++ BusinessLogic/ProductManager.cs
@@ -6,16 +6,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLogic
-{
+namespace BusinessLogic {  
     public class ProductManager
     {
-        public void AddProduct() {
-        
+        public void AddProduct(Product product) 
+        {  
+            if (product == null) throw new ArgumentNullException(nameof(product), "El producto no puede ser nulo");
+            if (string.IsNullOrWhiteSpace(product.Name)) throw new ArgumentException("El nombre del producto no puede estar vacio");
+            if (product.Price <= 0) throw new ArgumentException("El precio debe ser mayor a 0");
+            if (product.Stock < 0) throw new ArgumentException("El stock no puede ser negativo");
         }
     
-    
-        public  List<Product> GetProducts() 
+         public  List<Product> GetProducts() 
             { 
             var list=new List<Product>();
             return list;
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿public class Program
+﻿using BusinessLogic;
+using Entities;
+using System.Net.Http.Headers;
+
+public class Program
 {
        public static void Main(string[] args)
     {
@@ -12,14 +16,37 @@
 
         switch (option) {
             case 1:
-                Console.WriteLine("Digite la informacion del producto a continuar");     
+                Console.WriteLine("Digite la informacion del producto a continuar");
+                Console.WriteLine("Indique el Id:");
+                var id = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Indique el nombre:");
+                var name = Console.ReadLine();
+                Console.WriteLine("Indique la cantidad disponible");
+                var stock= Int32.Parse(Console.ReadLine());
+                Console.WriteLine();    
+                var price=Double.Parse(Console.ReadLine());
+
+                var myProduct = new Product() { Id = id, Name = name, Stock = stock, Price=price };
+
+                var pm = new ProductManager();
+
+                try
+                {
+                    pm.AddProduct(myProduct);
+
+                }
+                catch ( Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+               
                 break;
 
             case 2: Console.WriteLine("Listado de productos existentes");
                 break;
 
             default:
-                Console.WriteLine("Opcion incorrecta");
+                Console.WriteLine("Opcion incorrecta")
                 break;
 ;
 
